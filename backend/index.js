6568454f4c6a6e483991a9b92a0f0a45ca5fc746
grgnitika <gurungnitika59@gmail.com>
2025-07-03 01:09:35 +0545

    backend setup and database connected

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Database connection
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('✅ MongoDB database connected');
+  } catch (err) {
+    console.error('❌ MongoDB database connection failed:', err);
+  }
+};
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Server listen
+app.listen(port, () => {
+  connect();
+  console.log('🚀 Server listening on port', port);
+});
